5e91da68c0fc729b7fd5dd3887e3ee87259e5614
Uladzislau Sobal <sobolvladsav@gmail.com>
2024-11-15 20:17:12 +0300

    added modal validation, style fixes

--- web/src/modules/job-tracker/components/VacancyModal/VacancyModal.tsx
+++ web/src/modules/job-tracker/components/VacancyModal/VacancyModal.tsx
@@ -1,4 +1,13 @@
-import { Box, Button, Input, InputLabel, MenuItem, Select, TextField } from '@mui/material'
+import {
+  Box,
+  Button,
+  FormControl,
+  InputLabel,
+  MenuItem,
+  Select,
+  TextField,
+  Typography,
+} from '@mui/material'
 import * as React from 'react'
 
 import styles from './VacancyModal.module.css'
@@ -22,9 +31,18 @@ type VacancyModalUpdateProps = {
 type VacancyModalProps = VacancyModalCreateProps | VacancyModalUpdateProps
 
 const VacancyModal: React.FC<VacancyModalProps> = ({ hideModal, vacancy, mode }) => {
+  //need to display validation errors
+  const [isFormValidated, setIsFormValidated] = React.useState<boolean>(false)
+
   const [company, setCompany] = React.useState<string>(vacancy?.company ?? '')
+  const companyError = !company
+
   const [name, setName] = React.useState<string>(vacancy?.name ?? '')
+  const nameError = !name
+
   const [salary, setSalary] = React.useState<number>(vacancy?.salary ?? 0)
+  const salaryError = isNaN(+salary)
+
   const [status, setStatus] = React.useState<'active' | 'disabled'>(vacancy?.status ?? 'active')
   const [notes, setNotes] = React.useState<string>(vacancy?.notes ?? '')
 
@@ -33,6 +51,9 @@ const VacancyModal: React.FC<VacancyModalProps> = ({ hideModal, vacancy, mode })
 
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault()
+    //Set flag foe form validation
+    setIsFormValidated(true)
+    if (companyError || nameError || salaryError) return
     switch (mode) {
       case 'create':
         addVacancy({
@@ -58,29 +79,61 @@ const VacancyModal: React.FC<VacancyModalProps> = ({ hideModal, vacancy, mode })
   }
   return (
     <Modal onClose={() => hideModal()}>
-      <form onSubmit={handleSubmit} className={styles.form}>
-        <InputLabel>Company</InputLabel>
-        <Input id='company' value={company} onChange={(e) => setCompany(e.target.value)} />
-        <InputLabel>Name</InputLabel>
-        <Input id='name' value={name} onChange={(e) => setName(e.target.value)} />
-        <InputLabel>Salary</InputLabel>
-        <Input
+      <Box
+        component={'form'}
+        sx={{ display: 'flex', gap: 1 }}
+        onSubmit={handleSubmit}
+        className={styles.form}
+      >
+        <Typography style={{ textTransform: 'capitalize', fontSize: 24 }}>{mode}</Typography>
+        <TextField
+          size='small'
+          label={'Company'}
+          id='company'
+          value={company}
+          onChange={(e) => setCompany(e.target.value)}
+          error={isFormValidated && companyError}
+          helperText={isFormValidated && companyError ? 'Please enter company name!' : ''}
+        />
+        <TextField
+          size='small'
+          label={'Name'}
+          id='name'
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          error={isFormValidated && nameError}
+          helperText={isFormValidated && nameError ? 'Please enter vacancy name!' : ''}
+        />
+
+        <TextField
+          size='small'
+          label={'Salary'}
           id='salary'
           type='number'
           value={salary}
           onChange={(e) => setSalary(isNaN(+e.target.value) ? 0 : +e.target.value)}
+          error={isFormValidated && salaryError}
+          helperText={isFormValidated && salaryError ? 'Salary should be a number!' : ''}
+          onFocus={(e) => e.target.select()}
         />
-        <InputLabel>Status</InputLabel>
-        <Select
-          size='small'
-          value={status}
-          onChange={(e) => setStatus(e.target.value as 'active' | 'disabled')}
-        >
-          <MenuItem value={'active'}>Active</MenuItem>
-          <MenuItem value={'disabled'}>Disabled</MenuItem>
-        </Select>
-        <InputLabel>Notes</InputLabel>
+        <FormControl fullWidth>
+          <InputLabel id='select-status-label'>Status</InputLabel>
+          <Select
+            label='Status'
+            labelId='select-status-label'
+            aria-describedby='select-helper'
+            size='small'
+            value={status}
+            onChange={(e) => setStatus(e.target.value as 'active' | 'disabled')}
+          >
+            <MenuItem value={'active'}>Active</MenuItem>
+            <MenuItem value={'disabled'}>Disabled</MenuItem>
+          </Select>
+        </FormControl>
+
         <TextField
+          size='small'
+          label={'Notes'}
           multiline
           rows={4}
           id='notes'
@@ -92,7 +145,7 @@ const VacancyModal: React.FC<VacancyModalProps> = ({ hideModal, vacancy, mode })
             Ok
           </Button>
         </Box>
-      </form>
+      </Box>
     </Modal>
   )
 }
